bdab13b87205aa7734f444813e5e63a28f92e5f9
Zahnoun_Mouad <168291583+ChimexXy@users.noreply.github.com>
2025-04-10 23:52:37 +0100

    chimex

---
+++ gnl.h
@@ -0,0 +1,15 @@
+#ifndef GNL_H
+#define GNL_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+
+# endif
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+char *gnl(int fd);
+
+# endif
