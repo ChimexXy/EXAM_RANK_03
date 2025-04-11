235249738ff0bcee75870df9957bc6eaa8b2c99c
Zahnoun_Mouad <168291583+ChimexXy@users.noreply.github.com>
2025-04-10 23:11:18 -0400

    chimex

--- permutations.c
+++ permutations.c
@@ -1,105 +1,98 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void	ft_putchar(char c)
+void ft_swap(char *c1, char *c2)
 {
-	write(1, &c, 1);
+    char tmp = *c1;
+    *c1 = *c2;
+    *c2 = tmp;
 }
 
-void	ft_putstr(char *str)
+
+int ft_strlen(char *str)
 {
-	while (*str)
-		ft_putchar(*str++);
-	ft_putchar('\n');
+    int i = 0;
+    while(str[i])
+        i++;
+    return i;
 }
 
-int	ft_strlen(char *str)
+int check_double(char *str)
 {
-	int i = 0;
-	while (str[i])
-		i++;
-	return (i);
+    int i = 0;
+    while(str[i])
+    {
+        int j = i + 1;
+        while(str[j])
+        {
+            if(str[i] == str[j] && str[j])
+                return 0;
+            j++;
+        }
+        i++;
+    }
+    return 1;
 }
 
-void	swap(char *a, char *b)
+void    str_sorted(char *str)
 {
-	char tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int i = 0;
+    while(str[i])
+    {
+        int j = i + 1;
+        while(str[j])
+        {
+            if(str[i] > str[j])
+                ft_swap(&str[i], &str[j]);
+            j++;
+        }
+        i++;
+    }
 }
 
-void	sort_str(char *str, int len)
+int ft_permutition(char *str, int len)
 {
-	int i = 0;
-	while (i < len - 1)
-	{
-		int j = i + 1;
-		while (j < len)
-		{
-			if (str[i] > str[j])
-				swap(&str[i], &str[j]);
-			j++;
-		}
-		i++;
-	}
+    int i = len - 2;
+    while(i >= 0 && str[i + 1] < str[i])
+        i--;
+    if(i < 0)
+        return 0;
+    int j = len - 1;
+    while(str[j] < str[i])
+        j--;
+    ft_swap(&str[i], &str[j]);
+    int s = i + 1;
+    int e = len - 1;
+    while(s < e)
+    {
+        ft_swap(&str[s], &str[e]);
+        s++;
+        e--;
+    }
+    return 1;
 }
 
-// Find next lexicographical permutation
-int	next_perm(char *str, int len)
+int main(int ac, char **av)
 {
-	int i = len - 2;
+    if(ac != 2)
+        return 0;
+    int i = 0;
+    int len = ft_strlen(av[1]);
+    char *ptr = malloc(len + 1);
+    if (!ptr)
+        return 0;
+    while(av[1][i])
+    {
+        ptr[i] = av[1][i];
+        i++;
+    }
+    ptr[i] = '\0';
+    if (!check_double(ptr))
+        return 0;
+    str_sorted(ptr);
+    printf("%s\n", ptr);
+    while(ft_permutition(ptr, len))
+        printf("%s\n", ptr);
 
-	while (i >= 0 && str[i] >= str[i + 1])
-		i--;
-
-	if (i < 0)
-		return (0); // last permutation
-
-	int j = len - 1;
-	while (str[j] <= str[i])
-		j--;
-
-	swap(&str[i], &str[j]);
-
-	int start = i + 1;
-	int end = len - 1;
-
-	while (start < end)
-	{
-		swap(&str[start], &str[end]);
-		start++;
-		end--;
-	}
-	return (1);
-}
-
-int	main(int ac, char **av)
-{
-	if (ac != 2)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
-
-	int len = ft_strlen(av[1]);
-	char *perm = malloc(len + 1);
-	if (!perm)
-		return (1);
-	
-	int i = 0;
-	while (i < len)
-	{
-		perm[i] = av[1][i];
-		i++;
-	}
-	perm[i] = '\0';
-
-	sort_str(perm, len);
-	ft_putstr(perm);
-
-	while (next_perm(perm, len))
-		ft_putstr(perm);
-
-	free(perm);
-	return (0);
 }
