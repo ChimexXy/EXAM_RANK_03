314508d389810d6deb402dad2e907ae1a519acd7
Zahnoun_Mouad <168291583+ChimexXy@users.noreply.github.com>
2025-04-13 21:16:48 +0100

    chimex

--- powerset.c
+++ powerset.c
@@ -4,104 +4,102 @@
 
 int ft_atoi(char *str)
 {
-	int i = 0;
-	int res = 0;
-	int sig = 1;
-	if(str[i] == '-' || str[i] == '+')
-	{
-		if(str[i] == '-')
-			sig *= -1;
-		i++;
-	}
-	while(str[i] && (str[i] >= '0' || str[i] <= '9'))
-	{
-		res *= 10;
-		res += (str[i] - 48);
-		i++;
-	}
-	return (res * sig);
+    int i = 0;
+    int res = 0;
+    int sig = 1;
+
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sig *= -1;
+        i++;
+    }
+    while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+    {
+        res = res * 10 + (str[i] - '0');
+        i++;
+    }
+    return (res * sig);
 }
 
 int check_int(char **str, int ac)
 {
-	int j = 1;
-	if (!str)
-		return 0;
-	while (j < ac)
-	{
-		int i = 0;
-		while(str[j][i])
-		{
-			if ((str[j][i] < '0' || str[j][i] > '9'))
-				return 0;
-			i++;
-		}
-		j++;
-	}
-	return 1;
+    int j = 1;
+    while (j < ac)
+    {
+        int i = 0;
+        if (str[j][i] == '-' || str[j][i] == '+')
+            i++;
+        while (str[j][i])
+        {
+            if (str[j][i] < '0' || str[j][i] > '9')
+                return 0;
+            i++;
+        }
+        j++;
+    }
+    return 1;
 }
 
-void print_set(int *tab)
+void print_set(int *tab, int size)
 {
-	int i = 0;
-	while(tab[i])
-	{
-		printf("%d ", tab[i]);
-		i++;
-	}
-		printf("\n");
+    int i = 0;
+    while (i < size)
+    {
+        printf("%d ", tab[i]);
+        i++;
+    }
+    printf("\n");
 }
 
-void powerset(int *tab, int ac)
+void powerset(int *arr, int n, int target)
 {
-	int i = 1;
-	int r = 1;
-	int sum = 0;
-	while(i < ac)
-	{
-		int m = i;
-		int *set = malloc(sizeof(int) * ac);
-		if(tab[m])
-		{
-		int j = m + 1;
-			while(tab[j])
-			{
-				sum = tab[m] + tab[j];
-				set[0] = tab[i];
-				set[r] = tab[j];
-				if(sum > tab[0])
-				{
-					// free(set[r]);
-					sum -= tab[j];
-					// r--;
-				}
-				if(sum == tab[0])
-				{
-					print_set(set);
-					free(set);
-					break;
-				}
-				r++;
-				j++;
-			}
-		}
-		i++;
-	}
+    int max = 1 << n; // 2^n subsets
+    int mask = 1;
+
+    while (mask < max)
+    {
+        int i = 0;
+        int sum = 0;
+        int *set = malloc(sizeof(int) * n);
+        int idx = 0;
+
+        while (i < n)
+        {
+            if ((mask >> i) & 1)
+            {
+                sum += arr[i];
+                set[idx] = arr[i];
+                idx++;
+            }
+            i++;
+        }
+
+        if (sum == target)
+            print_set(set, idx);
+
+        free(set);
+        mask++;
+    }
 }
 
 int main(int ac, char **av)
 {
-	if (!check_int(av, ac))
-		return 1;
-	int i = 1;
-	int j = 0;
-	int *tab = malloc(sizeof(int) * ac);
-	while(i < ac)
-	{
-		tab[j] = ft_atoi(av[i]);
-		i++;
-		j++;
-	}
-	tab[j] = '\0';
-	powerset(tab, ac);
+    if (ac < 3 || !check_int(av, ac))
+        return 1;
+
+    int target = ft_atoi(av[1]);
+    int n = ac - 2;
+
+    int *arr = malloc(sizeof(int) * n);
+    int i = 0;
+    while (i < n)
+    {
+        arr[i] = ft_atoi(av[i + 2]);
+        i++;
+    }
+
+    powerset(arr, n, target);
+    free(arr);
+    return 0;
 }
